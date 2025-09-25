5c10cacd3542fd2b93fd51d25dfc25e3d402c143
urpigu <slashlemon@gmail.com>
2025-09-25 17:57:55 +0700

    Perbaiki main.dart, tambah screenshot, dan tambahkan folder basic_widget beserta text_widget.dart

---
+++ lib/basic_widget/text_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyTextWidget extends StatelessWidget {
+  const MyTextWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Nama saya A. A. Ngurah Sadhu Gunawan, sedang belajar Pemrograman Mobile",
+      style: TextStyle(color: Colors.red, fontSize: 14),
+      textAlign: TextAlign.center,
+    );
+  }
+}
